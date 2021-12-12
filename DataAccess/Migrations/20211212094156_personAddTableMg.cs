1557a7b60148a7b63d5dc0a34ec231430750dfe2
yucellsarikaya <2017141050@cumhuriyet.edu.tr>
2021-12-12 12:46:11 +0300

    migration eklendi

---
+++ DataAccess/Migrations/20211212094156_personAddTableMg.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class personAddTableMg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "nvarchar(24)", nullable: true),
+                    surname = table.Column<string>(type: "nvarchar(24)", nullable: true),
+                    department = table.Column<string>(type: "nvarchar(24)", nullable: true),
+                    salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
--- Entities/Concrete/Person.cs
+++ Entities/Concrete/Person.cs
@@ -1,17 +1,29 @@
 ﻿using Core.Entites;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Entities.Concrete
 {
     public class Person : IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
-        public string  name { get; set; }
+
+        [Column(TypeName = "nvarchar(24)")]
+        public string name { get; set; }
+
+        [Column(TypeName = "nvarchar(24)")]
         public string surname { get; set; }
+
+        [Column(TypeName = "nvarchar(24)")]
         public string department { get; set; }
-        public int  salary { get; set; }
+
+        [Column(TypeName = "int")]
+        public int salary { get; set; }
 
     }
 }
